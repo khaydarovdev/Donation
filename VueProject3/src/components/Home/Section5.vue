d304866334aea08a6b005fd440ce0eac0fbdaaaa
khaydarovdev <x281035@gmail.com>
2024-10-07 15:34:44 +0300

    section 5

---
+++ VueProject3/src/components/Home/Section5.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="sec5_full">
+		<section class="sec5">
+			<h1>Our Upcoming Events</h1>
+			<div class="jessore">
+				<div class="je">
+					<img src="../../assets/Group 8.png" alt="" class="jeimg" />
+				</div>
+				<div class="je">
+					<img src="../../assets/Group 8.png" alt="" class="jeimg" />
+				</div>
+				<div class="je">
+					<img src="../../assets/Group 8.png" alt="" class="jeimg" />
+				</div>
+			</div>
+		</section>
+	</div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+.sec5_full {
+	width: 100%;
+	height: 50vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+.sec5 {
+	width: 1440px;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: space-around;
+}
+.sec5 h1 {
+	font-size: 45px;
+}
+.jessore {
+	display: flex;
+	align-items: center;
+	gap: 30px;
+}
+.je {
+	overflow: hidden;
+	height: 215px;
+}
+.jeimg {
+	transition: all 0.4s;
+}
+.je:hover {
+	.jeimg {
+		transform: scale(1.1);
+	}
+}
+</style>
--- VueProject3/src/views/HomeView.vue
+++ VueProject3/src/views/HomeView.vue
@@ -3,20 +3,23 @@
 		<Section1 />
 		<Section2 />
 		<Section3 />
-		<Section4/>
+		<Section4 />
+		<Section5 />
 	</div>
 </template>
 <script>
 import Section1 from '../components/Home/Section1.vue';
 import Section2 from '../components/Home/Section2.vue';
 import Section3 from '../components/Home/Section3.vue';
-import Section4 from '../components/Home/Section4.vue'
+import Section4 from '../components/Home/Section4.vue';
+import Section5 from '../components/Home/Section5.vue';
 export default {
 	components: {
 		Section1,
 		Section2,
 		Section3,
 		Section4,
+		Section5,
 	},
 };
 </script>
